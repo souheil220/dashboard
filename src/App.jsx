79c7fa1bbb0de0569ef108785da478ac24190f2e
Souheil HADJ HABIB <souheil.hadjhabib@groupe-hasnaoui.com>
2023-08-10 16:33:42 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import "./App.css";
+
+function App() {
+  <div>
+    <BrowserRouter>
+      <div className="flex relatiive dark:bg-main-dark-"></div>
+    </BrowserRouter>
+  </div>;
+}
+
+export default App;
